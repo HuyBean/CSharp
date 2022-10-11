c568e729db4eaa4a5576a22a1ee0274234cfb153
HuyBean <nqhuy0711@gmail.com>
2022-10-11 17:20:51 +0700

    commit

---
+++ Begin/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Begin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Console.Write() 
+            // In ra không xuống dòng
+            // Console.Write("Hello World!\n");
+            // Console.Write(10);
+            // Console.Write(10.9f);
+            // Console.Write(true);
+            #endregion
+            #region Console.WriteLine();
+            // In ra kèm xuống dòng
+            Console.WriteLine("Hello World!\n");
+            Console.WriteLine(10);
+            Console.WriteLine(10.9f);
+            Console.WriteLine(true);
+            #endregion
+            int a = 5;
+            Console.Write("a = " );
+            Console.WriteLine(a);
+            // Console.WriteLine("a = " + a); In ra màn hình cộng dồn giá trị của a
+            // Console.WriteLine("a = {0}", a); In ra màn hình giá trị của a
+            Console.WriteLine("{0} {1} {2} {3}", 1, 2, 3, 4); // In ra màn hình giá trị tương ứng
+            #region Console.Read()
+            // Lệnh này không đọc được phím chức năng
+            //Console.WriteLine(Console.Read()); // đọc 1 ký tự từ bán phím bằng lệnh read sau đó in ra ký tự vừa được đọc theo mã ASCII
+            #endregion
+            #region Console.ReadLine()
+            Console.WriteLine(Console.ReadLine()); // đọc từ bán phím bằng lệnh readline đến khi gặp ký tự xuống dòng thì dừng
+            // sau đó in ra chuỗi ký tự vừa được đọc
+            #endregion
+            #region Console.ReadKey()
+            Console.ReadKey(); // Không truyền tham số thì mặc định là false
+            Console.ReadKey(false);// Hiển thị phím ẩn lên màn hình
+            Console.ReadKey(true); // Không hiển thị phím ẩn lên màn hìnhy
+            #endregion
+        } 
+    }
+}
---
+++ Var/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Var
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Ví dụ 1
+            // Console.WriteLine("Hello World!");
+            // int KieuSoNguyen = 5;
+            // float BienKieuSoThuc = 6.9f;
+            // string BienKieuChuoi = "HowKteam.com - Free Education";
+            // bool BienKieuLuanLy = true;
+            // char BienKieuKyTu = 'K';
+
+            // Console.WriteLine(KieuSoNguyen);
+            // Console.WriteLine(BienKieuSoThuc);
+            // Console.WriteLine(BienKieuChuoi);
+            // Console.WriteLine(BienKieuLuanLy);
+            // Console.WriteLine(BienKieuKyTu);
+
+            // int aNum = 6;
+            // int bNum = 9;
+            // int cNum = aNum + bNum;
+
+            // Console.WriteLine("a + b = c : {0} + {1} = {2}", aNum, bNum, cNum);
+
+            #endregion
+
+            #region Ví dụ 2
+            // byte BienByte = 3;
+            // short BienShort = 9;
+            // int BienInt = 10;
+            // long BienLong = 0;
+
+            // BienLong = BienByte;
+            // Console.WriteLine(" BienLong = " + BienLong);
+            // BienLong = BienInt;
+            // Console.WriteLine(" BienLong = {0}", BienLong);
+            // BienShort = BienByte;
+            // Console.WriteLine(" BienShort = " + BienShort);
+            // BienInt = BienShort;
+            // Console.WriteLine(" BienInt = " + BienInt);
+            #endregion
+
+            #region Hằng
+            // const int a = 15;
+            // const int x = a;
+            // Console.WriteLine(x);
+            #endregion
+
+            #region Parse()
+            // string s = "10";
+            // int k = int.Parse(s);
+            // double e = double.Parse("10.9");
+            // Console.WriteLine(k);
+            // Console.WriteLine(e);
+            #endregion
+            
+            #region TryParse()
+            // int Result;
+            // bool isSuccess;
+            // string Data1 = "10", Data2 = "Kteam";
+            // isSuccess = int.TryParse(Data1, out Result);
+            // Console.Write(isSuccess == true ? "Success !" : "Faled !");
+            // Console.WriteLine("Result = " + Result);
+            #endregion
+            
+            #region Convert
+            #endregion
+            Console.ReadKey(true);
+        }
+    }
+}
