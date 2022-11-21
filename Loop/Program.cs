6edae1dfa17a1f2957871a022f834c7d9e0ac2bc
HuyBean <nqhuy0711@gmail.com>
2022-11-21 18:02:00 +0700

    commit

--- Loop/Program.cs
+++ Loop/Program.cs
@@ -2,11 +2,32 @@
 
 namespace Loop
 {
+    public class Student
+    {
+        public int id;
+        public int age;
+    }
     class Program
     {
+        static int FindMax(int a, int b)
+        {
+            return ((a > b)) ? a : b;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int i = 0;
+            for (; i < 10;)
+            {
+                Console.WriteLine("Hello  ");
+                i++;
+            }
+            Console.WriteLine("Max is: {0}", FindMax(5, 9));
+
+            Student A = new Student();
+            A.age = 10;
+            A.id = 123;
+            Console.WriteLine("A: {0}", A.age);
+            Console.ReadKey();
         }
     }
 }
